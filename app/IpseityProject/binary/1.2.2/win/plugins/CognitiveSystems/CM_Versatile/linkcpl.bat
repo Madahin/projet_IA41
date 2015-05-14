e6a351638bb4fe7ddfcd5c0c0c49bd0c4abbd58e
Madahin <nico99999@hotmail.fr>
2015-05-14 12:36:53 +0000

    Suppression de ce projet inutilisable est incompilable de merde

--- app/IpseityProject/binary/1.2.2/win/plugins/CognitiveSystems/CM_Versatile/linkcpl.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-rem plcon.exe -f none -F none -g true -t "consult(['%2']),qsave_program('aiprg.exe',[goal='\$welcome',toplevel=prolog,init_file=none])"
-
-plcon.exe -O -t "consult(['%2']),qsave_program('aiprg.exe')"
-
-copy /B %1+aiprg.exe %3
-del aiprg.exe
